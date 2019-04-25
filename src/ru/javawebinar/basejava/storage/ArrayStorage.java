3108fa9fe228b131a04adff3bd74e1b314f72be6
innoware11 <innoware11@gmail.com>
2019-04-25 17:53:17 +0300

    Lesson04 HW03

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -10,49 +10,17 @@ import java.util.Arrays;
  */
 public class ArrayStorage extends AbstractArrayStorage {
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
+    @Override
+    protected void fillDeletedElement(int index) {
+        storage[index] = storage[size - 1];
     }
 
-    public void save(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index != -1)
-            System.out.println("Resume " + r.getUuid() + " already isExist");
-        else if (size >= STORAGE_LIMIT)
-            System.out.println("Storage overflow");
-        else {
-            storage[size()] = r;
-            size++;
-        }
+    @Override
+    protected void insertElement(Resume r, int index) {
+        storage[size] = r;
     }
 
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1)
-            System.out.println("Resume " + uuid + " not isExist");
-        else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
-    }
-
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index == -1)
-            System.out.println("Resume " + r.getUuid() + " not isExist");
-        else
-            storage[index] = r;
-    }
-
+    @Override
     protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid))
